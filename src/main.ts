f87f996b17fb8484ff5afc319be3e43331fd749e
Pedro Henrique <127359574+Phhenrique3@users.noreply.github.com>
2025-08-12 22:22:09 -0300

    feat: Create user password = bcryot

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  app.enableCors(); // opcional
+
+  const port = process.env.PORT ?? 3001;
+  await app.listen(port);
+  console.log(`🚀 Server running at http://localhost:${port}`);
 }
 void bootstrap();
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,31 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body, Get, Param, Put} from '@nestjs/common';
+import { Prisma, User as UserModel } from '@prisma/client';
+import { UserService } from './user.service';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+  constructor(private userService: UserService) {}
+
+  @Post('singup')
+  async signupUser(
+    @Body() userData: Prisma.UserCreateInput,
+  ): Promise<UserModel> {
+    return this.userService.createUser(userData);
+  }
+  
+  @Get(':id')
+  async getUser(@Param('id')id: string): Promise<UserModel | null> {
+    return this.userService.user({id: Number(id)});
+  }
+
+  @Put()
+  async updateUser(
+    @Body() userData: Prisma.UserUpdateInput,
+    @Param('id') id: string,
+  ): Promise<UserModel> {
+    return this.userService.updateUser({
+      where: { id: Number(id) },
+      data: userData,
+    });
+  }
+}
