e7594feb7cf021ea1dd96bf9d413b2a93182ff34
Pedro Henrique <127359574+Phhenrique3@users.noreply.github.com>
2025-08-14 22:44:05 -0300

    fix:  Autenticando um usuário

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { Prisma } from '@prisma/client';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private readonly authService: AuthService) {} // ✅ injeção via construtor
+
+  @Post('signin')
+  signin(@Body() body: Prisma.UserCreateInput) {
+    return this.authService.signin(body);
+  }
+}
